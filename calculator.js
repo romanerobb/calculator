51fd4f4ed86bab4623737163567af2c452e262e1
Romane Orlando Robb <mail@romanerobb.com>
2023-01-03 13:31:10 -0500

    First commit

---
+++ calculator.js
@@ -0,0 +1,29 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+  });
+
+app.post('/', (req, res) => {
+
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+
+  var result = num1 + num2;
+  
+    res.send('<h1>"Here is the result of your calculation " + result</h1>');
+  });
+
+  app.get('/about', (req, res) => {
+    res.send('<h1>This is a simple calculator created by Romane Orlando Robb.</h1>');
+  });
+
+  app.listen(3000, (req, res) => {
+    console.log('Server is running on port 3000');
+  });
